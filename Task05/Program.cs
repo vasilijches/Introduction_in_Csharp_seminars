b57f52aa4c83e518897cbafdc1869afed07238b2
vasilijchesnokov <vasilijchesnokov@yandex.ru>
2023-04-10 21:04:56 +0300

    Task05 was added

---
+++ Task05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до +N
+// Например:
+// 2 -> "-2, -1, 0, 1, 2"
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int stopNumber = -number;
+while(number >= stopNumber)
+{
+    Console.Write(-number);
+    if(number > stopNumber) {Console.Write(", ");}
+    number -= 1; //число уменьшается на единицу, чтобы уменьшая number добраться до -number
+}
