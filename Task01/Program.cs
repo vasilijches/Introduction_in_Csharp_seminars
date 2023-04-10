40191306a8f7511120576d7dc29bc77cccd11582
vasilijchesnokov <vasilijchesnokov@yandex.ru>
2023-04-10 15:55:15 +0300

    Task01 was added by corresponding code

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,20 @@
-﻿// Проверить, является ли одно целое число квадратом другого целого числа
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа
+// и проверяет, является ли первое число квадратом второго.
+// Например:
+//     a = 25, b =  5 -> да
+//     a =  2, b = 10 -> нет
+//     a =  9, b = -3 -> да
+//     a = -3, b =  9 -> нет
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
