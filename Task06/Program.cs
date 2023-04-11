8e3904b35776da9434a4c65a6b543989060f3015
vasilijchesnokov <vasilijchesnokov@yandex.ru>
2023-04-11 18:21:27 +0300

    Task06 was added

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число
+// и выдаёт, является ли число чётным
+// Например:
+// 4 -> да
+// -3 -> нет
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) { Console.WriteLine("Да"); }
+else { Console.WriteLine("Нет"); }
