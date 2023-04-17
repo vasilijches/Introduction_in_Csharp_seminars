8bc5bc41e640e0c2f3fcafe4e1ece3d36e9f5aec
vasilijchesnokov <vasilijchesnokov@yandex.ru>
2023-04-17 10:52:06 +0300

    Task22 was added

---
+++ Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая
+принимает на вход число (N)
+и выдаёт таблицу квадратов чисел от 1 до N
+1   1
+2   4
+3   9
+4   16
+*/
+
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+SqTab(num);
+
+
+void SqTab(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        System.Console.WriteLine($"{count, 5}   {count * count, 5}");
+        count++;
+    }
+}
